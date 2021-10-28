faa3295d9dd4d43645491165bc1e57677ef37ee1
Infandel <tomahawk92@gmail.com>
2021-10-28 23:10:56 +0500

    Finished part 4-2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 FULLSTACKOPEN2021 
 Making a course from part3
+Link to my online app is:
+https://polar-woodland-22053.herokuapp.com/
---
+++ part4/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const requestLogger = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(requestLogger)
+
+module.exports = app
