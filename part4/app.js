b1bfb85c6c2ecb64c5ccc22d51eb0e45ee0671ac
Infandel <tomahawk92@gmail.com>
2021-11-13 02:20:21 +0500

    Finished part 4-20

--- part4/app.js
+++ part4/app.js
@@ -3,8 +3,10 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
-const requestLogger = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -20,9 +22,14 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
-app.use(requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
