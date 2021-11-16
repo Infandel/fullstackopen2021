7a88f4a1cc1980ad14721a91a91074731199cf69
Infandel <tomahawk92@gmail.com>
2021-11-17 01:30:37 +0500

    Finished part 5-4

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,23 +1,125 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
 import blogService from './services/blogs'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
+import Togglable from './components/Togglable'
+import loginService from './services/login'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
+    blogService
+      .getAll()
+      .then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])  
+
+  const logOut = () => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <>        
+        <h1>Log in to application</h1>
+        <Notification message={errorMessage} />
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+              <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit" className="button">login</button>
+        </form>
+      </>
+    )
+  }
+
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
+    <>
+      <div className="user">
+        {user.name} logged-in
+      </div>
+      <div>
+        <button className="button" onClick={logOut}>Log out</button>
+      </div>           
+      <Notification message={errorMessage} />
+      <ToastContainer position="top-right"
+        autoClose={5000} 
+        hideProgressBar={false} 
+        newestOnTop={false} 
+        closeOnClick rtl={false} 
+        pauseOnFocusLoss draggable pauseOnHover />
+      <Togglable buttonLabel='Create new Blog'>
+        <BlogForm
+          setBlogs={setBlogs}
+          blogs={blogs}
+          setErrorMessage={setErrorMessage}
+        />
+      </Togglable>
+      <h1>Blogs</h1>
+      <ul>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </ul>  
+      <Footer />
+    </>
   )
 }
 
---
+++ part5/src/components/BlogForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import blogService from '../services/blogs'
+
+const BlogForm = ({ 
+  setBlogs, blogs, setErrorMessage, 
+}) => {
+
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newURL, setNewURL] = useState('')
+
+  const handleTitleChange = (event) => {
+    setNewTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setNewAuthor(event.target.value)
+  }
+
+  const handleURLChange = (event) => {
+    setNewURL(event.target.value)
+  }
+
+  const notify = (title, author) => toast(`A new blog ${title}! by ${author} added`);
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newURL,
+    }
+    blogService
+      .create(blogObject)
+        .then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          setNewTitle('')
+          setNewAuthor('')
+          setNewURL('')
+          notify(returnedBlog.title, returnedBlog.author)
+        })
+        .catch(e => {
+          setErrorMessage('Something wrong with input')
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+        })   
+  }
+  return (
+    <>
+      <h1>Create new Blog</h1>
+      <form onSubmit={addBlog}>
+        <div>
+          <label>Title</label>          
+          <input value={newTitle} type="text" onChange={handleTitleChange} />          
+        </div>
+        <div>
+          <label>Author</label>         
+          <input value={newAuthor} type="text" onChange={handleAuthorChange} />          
+        </div>
+        <div>
+          <label>URL</label>
+          <input value={newURL} type="text" onChange={handleURLChange} />          
+        </div>
+        <button type="submit" className="button">Create</button>
+      </form>
+    </>
+  )
+}
+
+export default BlogForm
